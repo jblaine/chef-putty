67a1eb6c3d31c9df5a67f0f34a4a0b532eed92f7
Jeff Blaine <jblaine@kickflop.net>
2013-09-18 09:31:11 -0400

    Include sha256 checksum for windows_package resource to reference

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default['putty']['version']      = "0.62"
 default['putty']['url']          = "http://the.earth.li/~sgtatham/putty/#{default['putty']['version']}/x86/putty-#{default['putty']['version']}-installer.exe"
 default['putty']['package_name'] = "PuTTY version #{default['putty']['version']}"
+default['putty']['checksum']     = 'dd9c87f5692509093670e101ebe4230f29b778ff90e30020fcbcd5d3e92a6440'
 
 default['putty']['home']    = "#{ENV['SYSTEMDRIVE']}\\putty"
